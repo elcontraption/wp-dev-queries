2abb9317f4831e7ccc9e3a433214ebc891f81dd5
Darin Reid <darinreid@gmail.com>
2015-08-07 15:15:26 -0700

    Adding change-url query

---
+++ change-url.sql
@@ -0,0 +1,32 @@
+# Settings
+SET @old_url = 'http://example.com';
+SET @new_url = 'http://example.dev';
+SET @db_prefix = 'wp_';
+
+# Set up options table query
+SET @options_query = CONCAT('UPDATE ', @db_prefix, 'options SET option_value = replace(option_value, ?, ?) WHERE option_name = "home" OR option_name = "siteurl"');
+
+# Set up posts table queries
+SET @posts_query_1 = CONCAT('UPDATE ', @db_prefix, 'posts SET guid = REPLACE (guid, ?, ?)');
+SET @posts_query_2 = CONCAT('UPDATE ', @db_prefix, 'posts SET post_content = REPLACE (guid, ?, ?)');
+
+# Set up postmeta table query
+SET @postmeta_query = CONCAT('UPDATE ', @db_prefix, 'postmeta SET meta_value = REPLACE (meta_value, ?, ?)');
+
+# Prepare queries
+PREPARE query_1 FROM @options_query;
+PREPARE query_2 FROM @posts_query_1;
+PREPARE query_3 FROM @posts_query_2;
+PREPARE query_4 FROM @postmeta_query;
+
+# Execute queries
+EXECUTE query_1 USING @old_url, @new_url;
+EXECUTE query_2 USING @old_url, @new_url;
+EXECUTE query_3 USING @old_url, @new_url;
+EXECUTE query_4 USING @old_url, @new_url;
+
+# Deallocate prepared queries
+DEALLOCATE PREPARE query_1;
+DEALLOCATE PREPARE query_2;
+DEALLOCATE PREPARE query_3;
+DEALLOCATE PREPARE query_4;
---
+++ readme.md
@@ -0,0 +1,7 @@
+# WP Dev Queries
+Useful SQL queries for WordPress development.
+
+- [Change the url](#change-the-url)
+
+## Change the URL
+WordPress hardcodes the absolute URL throughout the database. This query enables you to change the URL.
